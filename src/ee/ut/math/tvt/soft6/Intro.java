58a81ff12d8e798feea1adaf0783b584cd6adc97
alajal <anulajal@gmail.com>
2014-10-05 14:04:50 +0300

    creating project

---
+++ src/ee/ut/math/tvt/soft6/Intro.java
@@ -0,0 +1,12 @@
+package ee.ut.math.tvt.soft6;
+
+import java.util.Date;
+
+public class Intro {
+
+    public static void main(String[] args) {
+
+        System.out.println(new Date());
+        System.out.println("Tere.");
+    }
+}
---
+++ src/ee/ut/math/tvt/soft6/IntroUI.java
@@ -0,0 +1,15 @@
+package ee.ut.math.tvt.soft6;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IntroUI {
+    private String teamName;
+    private String teamLeader;
+    private String teamLeaderEmail;
+    private List teamMembers = new ArrayList();
+    private String softwareVersionNumber;
+
+
+
+}
