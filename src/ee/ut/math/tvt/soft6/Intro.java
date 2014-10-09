ce04c5c532cb2c6abf2eb5bcb276fb7d7d2431ce
alajal <anulajal@gmail.com>
2014-10-09 21:22:57 +0300

    Creating basic UI

--- src/ee/ut/math/tvt/soft6/Intro.java
+++ src/ee/ut/math/tvt/soft6/Intro.java
@@ -1,12 +1,49 @@
 package ee.ut.math.tvt.soft6;
 
-import java.util.Date;
+import java.awt.*;
+import java.io.IOException;
+
+import org.apache.log4j.Logger;
+
+import javax.swing.*;
 
 public class Intro {
 
-    public static void main(String[] args) {
+    private static final Logger log = Logger.getLogger(Intro.class);
 
-        System.out.println(new Date());
-        System.out.println("Tere.");
+    public static void main(String[] args) {
+        System.out.println(System.getProperty("java.class.path"));
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    createAndShowGUI();
+                } catch (IOException e) {
+                    log.error("IO problem.", e);
+                    throw new RuntimeException(e);
+                } catch (Throwable t) {
+                    log.error("Something went wrong.", t);
+                    throw t;
+                }
+            }
+        });
+    }
+
+    private static void createAndShowGUI() throws IOException {
+        IntroUI intro = new IntroUI();
+        intro.setTitle("Information");
+
+        //size of frame
+        int width = 500;
+        int height = 500;
+        intro.setSize(width, height);
+
+        //location of screen
+        Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        intro.setLocation((screen.width - width) / 2, (screen.height - height) / 2);
+
+        intro.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        intro.setVisible(true);
+        log.info("Great success - information window opened.");
     }
 }
--- src/ee/ut/math/tvt/soft6/IntroUI.java
+++ src/ee/ut/math/tvt/soft6/IntroUI.java
@@ -1,15 +1,85 @@
 package ee.ut.math.tvt.soft6;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.apache.log4j.Logger;
 
-public class IntroUI {
-    private String teamName;
-    private String teamLeader;
-    private String teamLeaderEmail;
-    private List teamMembers = new ArrayList();
-    private String softwareVersionNumber;
+import javax.swing.*;
+import java.awt.*;
+import java.io.*;
+import java.util.Properties;
+
+public class IntroUI extends JFrame {
+
+    private static final Logger log = Logger.getLogger(IntroUI.class);
+    JPanel panel;
+    JLabel teamName;
+    JLabel teamLeader;
+    JLabel teamLeaderEmail;
+    JLabel teamMembers;
+    JLabel logo;
+
+    public IntroUI() throws IOException {
+        createComponents();
+        addComponentsToPanel();
+        fillFields();
+    }
+
+    private void createComponents() {
+        //setting up container & making layout manager
+        this.panel = new JPanel(new GridLayout(6, 1));
+
+        this.teamName = new JLabel(" Team name: soft6");
+        this.teamLeader = new JLabel("Team leader: Anu");
+        this.teamLeaderEmail = new JLabel("Team leader email: anulajal@gmail.com");
+        this.teamMembers = new JLabel("Team members: Lembit Valgma, Erki Lomp, Andrey Vavilov, Anu Lajal");
+        this.logo = new JLabel(createImageIcon("GeekHead.jpg"));
+    }
+
+    private void addComponentsToPanel() {
+        panel.add(teamName);
+        panel.add(teamLeader);
+        panel.add(teamLeaderEmail);
+        panel.add(teamMembers);
+        panel.add(logo);
+        this.add(panel);
+    }
+
+    /**
+     * Returns an ImageIcon, or null if the path was invalid.
+     */
+    protected static ImageIcon createImageIcon(String path) {
+        ClassLoader classLoader = IntroUI.class.getClassLoader();//oskab faile leida, otsib classpathist; klassi classloaderilt
+        java.net.URL imgURL = classLoader.getResource(path); //kysitakse kus on "path" fail, saan asukoha
+
+        if (imgURL != null) {
+            return new ImageIcon(imgURL);
+        } else {
+            System.err.println("Couldn't find file: " + path);
+            return null;
+        }
+    }
+
+    public void fillFields() throws IOException {
+
+        Properties properties = new Properties();
+        loadProperties(properties);
+        properties.getProperty("Name");
 
 
+    }
 
+    private void loadProperties(Properties properties) throws IOException {
+        ClassLoader cl = getClass().getClassLoader();
+        InputStream resourceStream = cl.getResourceAsStream("application.properties"); //andsin nime, mida otsin
+        if (resourceStream == null) {
+            throw new AssertionError("Check that etc is in classpath");
+        }
+        try {
+            properties.load(resourceStream);
+        } finally {
+            resourceStream.close();
+        }
+    }
 }
+
+
+
