e49466b51b200620868179dd1e69a9e2dc44e9bb
alajal <anulajal@gmail.com>
2014-10-14 11:21:06 +0300

    Fixed Intro.java - code duplication removed

--- src/ee/ut/math/tvt/soft6/Intro.java
+++ src/ee/ut/math/tvt/soft6/Intro.java
@@ -30,37 +30,24 @@ public class Intro {
     		cui.run();
     	} else {
 
-    		IntroUI introUI = new IntroUI();
-    		introUI.setVisible(true);
-    		introUI.setAlwaysOnTop(true);
-
     		final SalesSystemUI ui = new SalesSystemUI(domainController);
-    		ui.setVisible(true);
+            ui.setVisible(true);
 
-    		introUI.setAlwaysOnTop(false);
-    		try {
-    			Thread.sleep(3000);
-    		} catch (InterruptedException e) {
-    			e.printStackTrace();
-    		}
-    		introUI.setVisible(false);
-    	}
-    	
-    	
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    createAndShowGUI();
-                } catch (IOException e) {
-                    log.error("IO problem.", e);
-                    throw new RuntimeException(e);
-                } catch (Throwable t) {
-                    log.error("Something went wrong.", t);
-                    throw t;
+            SwingUtilities.invokeLater(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        createAndShowGUI();
+                    } catch (IOException e) {
+                        log.error("IO problem.", e);
+                        throw new RuntimeException(e);
+                    } catch (Throwable t) {
+                        log.error("Something went wrong.", t);
+                        throw t;
+                    }
                 }
-            }
-        });
+            });
+    	}
     }
 
     private static void createAndShowGUI() throws IOException {
@@ -71,13 +58,21 @@ public class Intro {
         int width = 500;
         int height = 350;
         intro.setSize(width, height);
+        intro.setAlwaysOnTop(false);
 
         //location of screen
         Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
         intro.setLocation((screen.width - width) / 2, (screen.height - height) / 2);
-
         intro.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        intro.setVisible(true);
-        log.info("Great success - information window opened.");
+
+        intro.setVisible(false);
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        //log.info("Great success - information window opened.");
     }
 }
