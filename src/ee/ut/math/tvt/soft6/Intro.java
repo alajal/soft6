a70f53386e53e900e71429f4792f0802f805e441
valgma <lembit.valgma@gmail.com>
2014-10-14 10:43:24 +0300

    lab4 new stuff

--- src/ee/ut/math/tvt/soft6/Intro.java
+++ src/ee/ut/math/tvt/soft6/Intro.java
@@ -1,17 +1,52 @@
 package ee.ut.math.tvt.soft6;
 
-import java.awt.*;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 import java.io.IOException;
+
+import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
 
 import org.apache.log4j.Logger;
 
-import javax.swing.*;
+import ee.ut.math.tvt.salessystem.domain.controller.SalesDomainController;
+import ee.ut.math.tvt.salessystem.domain.controller.impl.SalesDomainControllerImpl;
+import ee.ut.math.tvt.salessystem.ui.ConsoleUI;
+import ee.ut.math.tvt.salessystem.ui.SalesSystemUI;
 
 public class Intro {
 
     private static final Logger log = Logger.getLogger(Intro.class);
+    private static final String MODE = "console";
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
+    	final SalesDomainController domainController = new SalesDomainControllerImpl();
+
+    	if (args.length == 1 && args[0].equals(MODE)) {
+    		log.debug("Mode: " + MODE);
+
+    		ConsoleUI cui = new ConsoleUI(domainController);
+    		cui.run();
+    	} else {
+
+    		IntroUI introUI = new IntroUI();
+    		introUI.setVisible(true);
+    		introUI.setAlwaysOnTop(true);
+
+    		final SalesSystemUI ui = new SalesSystemUI(domainController);
+    		ui.setVisible(true);
+
+    		introUI.setAlwaysOnTop(false);
+    		try {
+    			Thread.sleep(3000);
+    		} catch (InterruptedException e) {
+    			e.printStackTrace();
+    		}
+    		introUI.setVisible(false);
+    	}
+    	
+    	
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
