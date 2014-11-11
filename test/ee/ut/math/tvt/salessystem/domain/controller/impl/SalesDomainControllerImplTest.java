bbeb24645e838df9f1c69e8bac077572302271ad
valgma <lembit.valgma@gmail.com>
2014-11-11 11:59:03 +0200

    Files/organization for testing: - junit.jar -> must be added to classpath - test folder with sample class -> test folder must be added to classpath - build.xml, added paths, targets

---
+++ test/ee/ut/math/tvt/salessystem/domain/controller/impl/SalesDomainControllerImplTest.java
@@ -0,0 +1,19 @@
+package ee.ut.math.tvt.salessystem.domain.controller.impl;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SalesDomainControllerImplTest {
+	
+	@Before
+	public void setUp() {
+		
+	}
+	
+	@Test
+	public void exampleTest() {
+		assertEquals(1.0, 1.0, 0.0001);
+	}
+
+}
