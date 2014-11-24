2748a9147b0636096125aedb3c0df9a8ce65a94c
alajal <anulajal@gmail.com>
2014-11-24 03:19:56 +0200

    FakeDataService refactored

--- test/ee/ut/math/tvt/salessystem/FakeDataService.java
+++ test/ee/ut/math/tvt/salessystem/FakeDataService.java
@@ -24,28 +24,12 @@ public class FakeDataService extends HibernateDataService {
 
     @Override
     public List<StockItem> getStockItems() {
-        StockItem s = new StockItem(3L, "Kapsasalat", "salat", 2, 6);
-        List<StockItem> stockItemList = new ArrayList<>();
-        //stockItemList.add(s);
-        return stockItemList;
+        return new ArrayList<>();
     }
 
     @Override
     public List<Order> getOrders() {
-        List<SoldItem> soldItems1 = new ArrayList<>();
-        //List<SoldItem> soldItems1 = new ArrayList<>();
-
-        StockItem s = new StockItem(3L, "Kapsasalat", "salat", 5, 6);
-        StockItem d = new StockItem((long) 1, "Ice-cream", "vanilla ice-cream", 4, 10);
-        SoldItem soldItem1 = new SoldItem(s,1);
-        SoldItem soldItem2 = new SoldItem(d,2);
-        SoldItem soldItem3 = new SoldItem(s,2);
-        SoldItem soldItem4 = new SoldItem(d,1);
-        soldItems1.add(soldItem1);
-        //Order order1 = new Order(orderedItems, new Date());
-        List<Order> orders = new ArrayList<>();
-
-        return  orders;
+        return new ArrayList<>();
     }
 
     @Override
